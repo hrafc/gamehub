5f329a29f24c9fa89885b2b1bbba80527c15c557
hrafc <otagillug@gmail.com>
2026-05-03 18:37:21 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "gamehub-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./game1.html",
+  "./game2.html",
+  "./game3.html",
+  "./elevator.mp3",
+  "./logochess.png",
+  "./logocheckers.png",
+  "./logocrosssum.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
